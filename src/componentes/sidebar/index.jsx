7fa5f4edaae99b212c3c4ad74649997fb9a12123
Pedro <pedrohenriquefrederi@gmai.com>
2025-07-02 17:20:04 -0300

    primeiro commit escopo sidebar

---
+++ src/componentes/sidebar/index.jsx
@@ -0,0 +1,8 @@
+export default function Sidebar () {    //definimos que deste arquivo sera exportada a função Sidebar
+    return (                                //componentes que sao formados por uma função
+        
+
+
+
+    )
+}
