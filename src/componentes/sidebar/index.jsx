2b9e6e9721c838b2df67c6b5a53de0e024ab6fee
Pedro <pedrohenriquefrederi@gmai.com>
2025-07-02 17:30:17 -0300

    Componentizacao SideBar

--- src/componentes/sidebar/index.jsx
+++ src/componentes/sidebar/index.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar () {    //definimos que deste arquivo sera exportada a função Sidebar
     return (                                //componentes que sao formados por uma função
-        
+                                                //componentes do feed serao adicionados aqui
 
 
 
