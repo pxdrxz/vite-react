0dc3ac6097a0d06d478b45fea45699f505dfe787
Pedro <pedrohenriquefrederi@gmai.com>
2025-07-14 16:42:11 -0300

    Estilização Sidebar V2

--- src/componentes/sidebar/index.tsx
+++ src/componentes/sidebar/index.tsx
@@ -4,36 +4,37 @@ import Logout from '../assets/Logout.svg';
 import Perfil from '../assets/account_circle.svg';
 import Info from '../assets/info.svg';
 // import LogoName from './assets/codeconnect.svg';
+import '../styles.css';
 
 export default function Sidebar() {    //definimos que deste arquivo sera exportada a função Sidebar
     return (                                //componentes que sao formados por uma função 
         <aside>
             <img src={Logo} alt='Logo do CodeConnect' />
             <nav>
-                <ul>
+                <ul className="lista-sidebar">
                     <li>
-                        <a href='#'>Publicar</a>
+                        <a href='#' className='item__link-publicacao'>Publicar</a>
                     </li>
                     <li>
-                        <a href='#'>
+                        <a href='#' className='item__link'>
                         <img src={Perfil} alt='' /> 
                         <span>Perfil</span>
-                    </a>
+                    </a>    
                 </li>
                     <li>
-                    <a href='#'>
+                    <a href='#' className='item__link item__link--ativo'>
                         <img src={Feed} alt='' /> 
                         <span>Feed</span>
                     </a>
                 </li>
                     <li>
-                        <a href='#'>
+                        <a href='#' className='item__link'>
                             <img src={Info} alt='' />
                             <span>Sobre Nós</span>
                         </a>
                     </li>
                         <li>
-                            <a href='#'>
+                            <a href='#' className='item__link'>
                             <img src={Logout} alt='' />
                             <span>Sair</span>
                         </a>
