2ff6dda0f89e0b50db95d94f7228a1d7a775603c
Pedro <pedrohenriquefrederi@gmai.com>
2025-07-28 18:54:04 -0300

    Estilização Cards V2 - CSS colunas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Sidebar from './componentes/sidebar/index';
 import BarraDePesquisa from './componentes/BarraDePesquisa/index.tsx';
 import Filtro from './componentes/Filtros/index.tsx';
 import Card from './componentes/card/index.tsx';
 
+
 function App() {
+  const [dados, setDados] = useState ([]);
+  useEffect(() => {
+    console.log(dados)
+    fetch ('https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes')
+      .then(resposta => resposta.json())
+      .then(dados =>setDados(dados))
+  }, [])
  
   return ( 
     <div className='container'>
@@ -12,7 +21,22 @@ function App() {
       <div>
         <BarraDePesquisa/>
         <Filtro/>  
-        <Card/>
+        <ul className="lista-cards">
+      {dados ? dados.map((item, index) => (
+    <li key={index}>
+      <Card 
+        id={item.id}
+        imagemUrl={item.imagem_capa}
+        titulo={item.titulo}
+        resumo={item.resumo}
+        LinhasDeCodigo={item.linhas_de_codigo}
+        compartilhamentos={item.compartilhamentos}
+        comentarios={item.comentarios}
+        usuario={item.usuarios}
+        />
+        </li>
+      )) :null}
+      </ul>
     </div>
   </div>
   )
--- src/componentes/card/index.tsx
+++ src/componentes/card/index.tsx
@@ -1,38 +1,39 @@
-import Capa from './assets/Code editor.png';
+//import Capa from './assets/Code editor.png';
 import Chat from './assets/chat.svg';
 import Code from './assets/code.svg';
 import Share from './assets/share.svg';
-import Icone from './assets/userphoto.png';
+//import Icone from './assets/userphoto.png';
 import './styles.css'
 
-export default function Card() { 
+export default function Card({id, imagemUrl, titulo, resumo, LinhasDeCodigo, compartilhamentos, comentarios, usuario}) { 
     return (
         <article className="card">
             <div className="card__imagem">
-                <img src={Capa} alt="Imagem do Card"/>
+                <img src={imagemUrl} alt="Imagem do Card"/>
             </div>
                 <div className="card__conteudo">
                 <div className="conteudo__texto">
-                    <h3>Titulo do post</h3>
-                    <p>Resumo do post</p>    
+                    <h3>{titulo}</h3>
+                    <p>{resumo}</p>    
                 </div>
                 <div className="conteudo__rodape">
                     <ul>
                         <li>
                             <img src={Code} alt="Códigos"/>
-                            10
+                            {LinhasDeCodigo}
                         </li>
                         <li>    
-                            <img src={Chat} alt="Mensagens"/>
-                            15
+                            <img src={Chat} alt="Comentarios"/>
+                            {comentarios}
                         </li>
                         <li>
                             <img src={Share} alt="Compartilhamentos"/>
-                            18
+                            {compartilhamentos}
                         </li>
                     </ul>
                     <div className="rodape__usuario">
-                        <img src={Icone} alt="Icone do usuário"/>
+                        <img src={usuario} alt="Icone do usuário"/>
+                        {usuario}
                     </div>
                 </div>
             </div>
